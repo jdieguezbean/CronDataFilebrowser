aff35bbf29fbe78e04f3b2915d7aeb59d9241860
Jorge <jdieguezbean@gmail.com>
2021-06-05 17:06:28 +0300

    Working bug fix

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ RUN apk --update add ca-certificates \
 HEALTHCHECK --start-period=2s --interval=5s --timeout=3s \
   CMD curl -f http://localhost:8081/health || exit 1
 
+VOLUME /srv
 EXPOSE 8081
 
 COPY .docker.json /.filebrowser.json
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/sh
+
+cd /
 chmod 777 filebrowser
-chmod 777 -R /etc/prometheus/alerts/
-/bin/bash ./filebrowser config init
-/bin/bash ./filebrowser config set --auth.method=noauth
-/bin/bash ./filebrowser -a 0.0.0.0 -p 8081 -b /etc/prometheus/alerts/
+./filebrowser config init
+./filebrowser config set --auth.method=noauth
+./filebrowser -a 0.0.0.0 -p 8081 -b /srv
