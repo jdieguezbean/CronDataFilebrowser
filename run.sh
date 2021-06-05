c3b64e7984231948fa35a4114b47d27a4575f68c
Jorge <jdieguezbean@gmail.com>
2021-06-05 15:06:40 +0300

    Adding /bin/bash to run filebrowser command

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 cd /
 chmod 777 filebrowser
 chmod 777 -R /etc/prometheus/alerts/
-./filebrowser config init
-./filebrowser config set --auth.method=noauth
-./filebrowser -a 0.0.0.0 -p 8081 -b /etc/prometheus/alerts/
+/bin/bash ./filebrowser config init
+/bin/bash ./filebrowser config set --auth.method=noauth
+/bin/bash ./filebrowser -a 0.0.0.0 -p 8081 -b /etc/prometheus/alerts/
