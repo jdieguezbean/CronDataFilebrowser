ca48fb76f35f3a32cbc94ef488f60cc60bcc639b
Jorge <jdieguezbean@gmail.com>
2021-06-07 23:06:11 +0300

    Fixing bug in run image, adding css, adding poppins

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@
 cd /
 chmod 777 filebrowser
 ./filebrowser config init
+./filebrowser users add admin admin
 ./filebrowser config set --auth.method=noauth
-./filebrowser -a 0.0.0.0 -p 8081 -b /srv
+./filebrowser -a 0.0.0.0 -p 8081 -r /etc/prometheus/alerts
